dbf076f10b036c19c96f89f535ad8ca72dac537b
Maksim Varkus <maksim.varkus@eviden.com>
2024-08-07 12:03:49 +0100

    Implement "latest large producers register" feature

--- src/FrontendObligationChecker.UnitTests/Controllers/LargeProducerRegisterControllerTests.cs
+++ src/FrontendObligationChecker.UnitTests/Controllers/LargeProducerRegisterControllerTests.cs
@@ -1,12 +1,12 @@
 ﻿namespace FrontendObligationChecker.UnitTests.Controllers;
 
-using Constants;
 using Exceptions;
 using FluentAssertions;
+using FrontendObligationChecker.Constants;
 using FrontendObligationChecker.Controllers;
-using FrontendObligationChecker.Services.Caching;
 using FrontendObligationChecker.Services.LargeProducerRegister.Interfaces;
 using FrontendObligationChecker.ViewModels;
+using FrontendObligationChecker.ViewModels.LargeProducer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -23,18 +23,16 @@ public class LargeProducerRegisterControllerTests
     private Mock<ISession> _sessionMock;
 
     private LargeProducerRegisterController _systemUnderTest;
-    private Mock<ICacheService> _cacheServiceMock;
 
     [TestInitialize]
     public void TestInitialize()
     {
         _largeProducerRegisterService = new Mock<ILargeProducerRegisterService>();
         _logger = new Mock<ILogger<LargeProducerRegisterController>>();
-        _cacheServiceMock = new Mock<ICacheService>();
         _sessionMock = new Mock<ISession>();
         _httpContextMock = new Mock<HttpContext>();
         _httpContextMock.Setup(x => x.Session).Returns(_sessionMock.Object);
-        _systemUnderTest = new LargeProducerRegisterController(_largeProducerRegisterService.Object, _logger.Object, _cacheServiceMock.Object);
+        _systemUnderTest = new LargeProducerRegisterController(_largeProducerRegisterService.Object, _logger.Object);
         _systemUnderTest.ControllerContext.HttpContext = _httpContextMock.Object;
     }
 
@@ -42,122 +40,84 @@ public class LargeProducerRegisterControllerTests
     public async Task Get_ReturnsLargeProducerRegisterView_WhenCalled()
     {
         // Arrange
-        Dictionary<string, string> expectedCacheValue = new();
-        _cacheServiceMock.Setup(c => c.GetReportFileSizeCache(It.IsAny<string>(), out expectedCacheValue))
-            .Returns(false);
-        _largeProducerRegisterService.Setup(x => x.GetAllReportFileSizesAsync(It.IsAny<string>()))
-            .ReturnsAsync(GetFileSizeMappingDictionary());
+        var expected = new LargeProducerFileInfoViewModel
+        {
+            DisplayFileSize = "2MB",
+            DateCreated = new DateTime(1970, 1, 1)
+        };
+
+        _largeProducerRegisterService.Setup(x => x.GetLatestAllNationsFileInfoAsync(It.IsAny<string>()))
+            .ReturnsAsync(expected);
 
         // Act
         var result = await _systemUnderTest.Get() as ViewResult;
 
         // Assert
         result.Should().NotBeNull();
-        LargeProducerRegisterViewModel viewModel = (LargeProducerRegisterViewModel)result.ViewData.Model;
-        viewModel.HomeNationFileSizeMapping.Should().BeEquivalentTo(GetFileSizeMappingDictionary());
+        LargeProducerRegisterViewModel viewModelResult = (LargeProducerRegisterViewModel)result.ViewData.Model;
+        viewModelResult.LatestAllNationsFile.Should().BeEquivalentTo(expected);
         result.ViewName.Should().Be("LargeProducerRegister");
-        _largeProducerRegisterService.Verify(x => x.GetAllReportFileSizesAsync(It.IsAny<string>()), Times.Once);
+        _largeProducerRegisterService.Verify(x => x.GetLatestAllNationsFileInfoAsync(It.IsAny<string>()), Times.Once);
     }
 
     [TestMethod]
-    public async Task Get_ReturnsLargeProducerRegisterView_WhenCalledAndCacheExists()
+    public async Task GetFile_ValidNationCodeAndLargeProducerRegisterServiceReturnsFile_ReturnFile()
     {
         // Arrange
-        _largeProducerRegisterService.Setup(x => x.GetAllReportFileSizesAsync(It.IsAny<string>()))
-            .ReturnsAsync((Dictionary<string, string>)null);
-        Dictionary<string, string> expectedCacheValue = GetFileSizeMappingDictionary();
-        _cacheServiceMock.Setup(c => c.GetReportFileSizeCache(It.IsAny<string>(), out expectedCacheValue))
-            .Returns(true);
+        var viewModel = new LargeProducerFileViewModel
+        {
+            FileName = "example error report.csv",
+            FileContents = new MemoryStream()
+        };
+
+        _largeProducerRegisterService.Setup(x => x.GetLatestAllNationsFileAsync(It.IsAny<string>())).ReturnsAsync(viewModel);
 
         // Act
-        var result = await _systemUnderTest.Get() as ViewResult;
+        var result = await _systemUnderTest.File() as FileStreamResult;
 
         // Assert
-        LargeProducerRegisterViewModel viewModel = (LargeProducerRegisterViewModel)result.ViewData.Model;
-        viewModel.HomeNationFileSizeMapping.Should().BeEquivalentTo(GetFileSizeMappingDictionary());
-        result.Should().NotBeNull();
-        result.ViewName.Should().Be("LargeProducerRegister");
-    }
-
-    [TestMethod]
-    [DataRow(HomeNation.England)]
-    [DataRow(HomeNation.NorthernIreland)]
-    [DataRow(HomeNation.Scotland)]
-    [DataRow(HomeNation.Wales)]
-    [DataRow(HomeNation.All)]
-    public async Task GetFile_ValidNationCodeAndLargeProducerRegisterServiceReturnsFile_ReturnFile(string nationCode)
-    {
-        // Arrange
-        const string fileName = "example error report.csv";
-        var memoryStream = new MemoryStream();
-
-        _largeProducerRegisterService.Setup(x => x.GetReportAsync(It.IsAny<string>(), It.IsAny<string>()))
-            .ReturnsAsync((memoryStream, fileName));
-
-        // Act
-        var result = await _systemUnderTest.File(nationCode) as FileStreamResult;
-
-        // Assert
-        result.FileDownloadName.Should().Be(fileName);
-        result.FileStream.Should().BeSameAs(memoryStream);
+        result.FileDownloadName.Should().Be(viewModel.FileName);
+        result.FileStream.Should().BeSameAs(viewModel.FileContents);
         result.ContentType.Should().Be("text/csv");
     }
 
     [TestMethod]
-    [DataRow(HomeNation.England)]
-    [DataRow(HomeNation.NorthernIreland)]
-    [DataRow(HomeNation.Scotland)]
-    [DataRow(HomeNation.Wales)]
-    [DataRow(HomeNation.All)]
-    public async Task GetFile_LargeProducerRegisterServiceThrowsException_RedirectToLargeProducerErrorPage(string nationCode)
+    public async Task GetFile_LargeProducerRegisterServiceThrowsException_RedirectToLargeProducerErrorPage()
     {
         // Arrange
-        _largeProducerRegisterService.Setup(x => x.GetReportAsync(It.IsAny<string>(), It.IsAny<string>()))
+        _largeProducerRegisterService.Setup(x => x.GetLatestAllNationsFileAsync(It.IsAny<string>()))
             .ThrowsAsync(new LargeProducerRegisterServiceException());
 
         // Act
-        var result = await _systemUnderTest.File(nationCode) as RedirectToActionResult;
+        var result = await _systemUnderTest.File() as RedirectToActionResult;
 
         // Assert
         result.ActionName.Should().Be("FileNotDownloaded");
-        _logger.VerifyLog(logger => logger.LogError(FileNotDownloadedExceptionLog, nationCode), Times.Once);
+        _logger.VerifyLog(logger => logger.LogError(FileNotDownloadedExceptionLog, HomeNation.All), Times.Once);
     }
 
     [TestMethod]
-    [DataRow(HomeNation.England)]
-    [DataRow(HomeNation.NorthernIreland)]
-    [DataRow(HomeNation.Scotland)]
-    [DataRow(HomeNation.Wales)]
-    [DataRow(HomeNation.All)]
-    public async Task GetFileNotDownloaded_ReturnsLargeProducerErrorView_WhenCalledWithValidHomeNation(string nationCode)
+    public async Task GetFile_FileNotFound_RedirectToLargeProducerErrorPage()
+    {
+        // Arrange
+        _largeProducerRegisterService.Setup(x => x.GetLatestAllNationsFileAsync(It.IsAny<string>()))
+            .ReturnsAsync((LargeProducerFileViewModel)null);
+
+        // Act
+        var result = await _systemUnderTest.File() as RedirectToActionResult;
+
+        // Assert
+        result.ActionName.Should().Be("FileNotDownloaded");
+    }
+
+    [TestMethod]
+    public async Task GetFileNotDownloaded_ReturnsLargeProducerErrorView_WhenCalledWithValidHomeNation()
     {
         // Act
-        var result = await _systemUnderTest.FileNotDownloaded(nationCode) as ViewResult;
+        var result = await _systemUnderTest.FileNotDownloaded() as ViewResult;
 
         // Assert
         result.Should().NotBeNull();
         result.ViewName.Should().Be("LargeProducerError");
-    }
-
-    private static Dictionary<string, string> GetFileSizeMappingDictionary()
-    {
-        return new Dictionary<string, string>()
-        {
-            {
-                HomeNation.England, "10MB"
-            },
-            {
-                HomeNation.Scotland, "10KB"
-            },
-            {
-                HomeNation.Wales, "10MB"
-            },
-            {
-                HomeNation.NorthernIreland, "10MB"
-            },
-            {
-                HomeNation.All, "10MB"
-            }
-        };
     }
 }
