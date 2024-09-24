9b2528690e1f7eedc60a3e1b097597e392b04612
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-24 20:38:30 +0100

    error page fix

---
+++ src/FrontendObligationChecker/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace FrontendObligationChecker.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime UtcToGmt(this DateTime dateTime) => TimeZoneInfo.ConvertTimeFromUtc(dateTime, TimeZoneInfo.FindSystemTimeZoneById("Europe/London"));
+    }
+}
