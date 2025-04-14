0de191b39d51f9a0db6fb50d9debe52cffa6c6f5
prasad.ramireddy@defra.onmicrosoft.com <prasad.ramireddy@defra.onmicrosoft.com>
2025-04-14 22:35:28 +0100

    Added code changes to read Public register blobs

--- src/FrontendObligationChecker/Controllers/PublicRegisterController.cs
+++ src/FrontendObligationChecker/Controllers/PublicRegisterController.cs
@@ -3,6 +3,8 @@
     using System.Globalization;
     using FrontendObligationChecker.Constants;
     using FrontendObligationChecker.Constants.PublicRegister;
+    using FrontendObligationChecker.Models.BlobReader;
+    using FrontendObligationChecker.Services.PublicRegister;
     using FrontendObligationChecker.ViewModels.PublicRegister;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.FeatureManagement.Mvc;
@@ -11,16 +13,29 @@
     [Route(PagePath.PublicRegister)]
     public class PublicRegisterController : Controller
     {
+        private IBlobStorageService blobStorageService;
+
+        public PublicRegisterController(IBlobStorageService blobStorageService)
+        {
+            this.blobStorageService = blobStorageService;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Guidance()
         {
-            // This is hard-coded for the sake of displaying the view for story #523624
+            PublicRegisterBlobModel blobModel = await blobStorageService.GetLatestProducersFilePropertiesAsync();
+
+            string publishedDate = blobModel.PublishedDate.ToString("d MMMM yyyy", CultureInfo.InvariantCulture);
+            string lastUpdated = blobModel.LastModified?.ToString("d MMMM yyyy", CultureInfo.InvariantCulture) ?? publishedDate;
+            string producersRegisteredFileSize = blobModel.ContentLength?.ToString() ?? "0";
+
+            // This is hard-coded cso data for the sake of displaying the view for story #523624
             var viewModel = new GuidanceViewModel
             {
-                ComplianceSchemesRegisteredFileSize = "450",
-                LastUpdated = new DateTime(2025, 3, 10, 0, 0, 0, DateTimeKind.Utc).ToString("d MMMM yyyy", CultureInfo.InvariantCulture),
-                ProducersRegisteredFileSize = "115",
-                PublishedDate = new DateTime(2025, 12, 6, 0, 0, 0, DateTimeKind.Utc).ToString("d MMMM yyyy", CultureInfo.InvariantCulture)
+                PublishedDate = publishedDate,
+                LastUpdated = lastUpdated,
+                ProducersRegisteredFileSize = producersRegisteredFileSize,
+                ComplianceSchemesRegisteredFileSize = "450"
             };
 
             return View("Guidance", viewModel);
