99ff85f90044bafcd3d27b1efad7a09c84bc32c0
Hamzah Shakeel <hamzah.shakeel.external@atos.net>
2025-09-17 09:19:46 +0100

    updated partial views to align with new frontend templates and styling (#79)
    updated gulpfile to silence known deprecation warnings
    
    updated structure around wwwroot folders generated to define a mroe consistent approach
    
    now using the new govuk blue themed styling

--- src/FrontendObligationChecker/gulpfile.js
+++ src/FrontendObligationChecker/gulpfile.js
@@ -1,37 +1,65 @@
-const gulp = require('gulp')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify')
-const sass = require('gulp-sass')(require('sass'))
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+const path = require('path');
+
+const paths = {
+    govuk: "node_modules/govuk-frontend/dist/govuk/",
+    scss: [
+        'assets/scss/application.scss'
+    ],
+    output: 'wwwroot/css'
+};
+
+const deprecationSuppressions = ["import", "mixed-decls", "global-builtin"];
+
+let loadPaths = [
+    path.join(__dirname, "node_modules"),
+    path.join(__dirname, paths.govuk)
+];
+
+const sassOptions = {
+    loadPaths: loadPaths,
+    outputStyle: 'compressed',
+    quietDeps: true,
+    silenceDeprecations: deprecationSuppressions
+};
 
 gulp.task('compile-scss', () => {
-  return gulp.src('assets/scss/application.scss')
-    .pipe(sass({outputStyle: 'compressed'}, ''))
-    .pipe(gulp.dest('wwwroot/css', { overwrite: true }))
-})
+  return gulp.src(paths.scss)
+    .pipe(sass(sassOptions, ''))
+    .pipe(gulp.dest('wwwroot/css', { overwrite: true }));
+});
 
 gulp.task('copy-fonts', () => {
-  return gulp.src('node_modules/govuk-frontend/govuk/assets/fonts/*')
-    .pipe(gulp.dest('wwwroot/fonts', { overwrite: true }))
-})
+  return gulp.src(path.join(paths.govuk, '/assets/fonts/*'))
+    .pipe(gulp.dest('wwwroot/fonts', { overwrite: true }));
+});
 
 gulp.task('copy-images', () => {
-  return gulp.src('node_modules/govuk-frontend/govuk/assets/images/*')
-    .pipe(gulp.dest('wwwroot/images', { overwrite: true }))
-})
+    return gulp.src(path.join(paths.govuk, '/assets/images/*'))
+    .pipe(gulp.dest('wwwroot/images', { overwrite: true }));
+});
 
 gulp.task('copy-govuk-javascript', () => {
-  return gulp.src('node_modules/govuk-frontend/govuk/all.js')
+  return gulp.src(path.join(paths.govuk, '/all.bundle.js'))
     .pipe(uglify())
     .pipe(rename('govuk.js'))
-    .pipe(gulp.dest('wwwroot/js', { overwrite: true }))
-})
+    .pipe(gulp.dest('wwwroot/js', { overwrite: true }));
+});
 
 gulp.task('copy-custom-javascript', () => {
   return gulp.src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('wwwroot/js', { overwrite: true }))
-})
+    .pipe(gulp.dest('wwwroot/js', { overwrite: true }));
+});
 
-gulp.task('copy-javascript', gulp.series('copy-govuk-javascript', 'copy-custom-javascript'))
+gulp.task('copy-rebrand', () => {
+    return gulp.src(path.join(paths.govuk, '/assets/rebrand/**/*'), { base: path.join(paths.govuk, '/assets/rebrand') })
+        .pipe(gulp.dest('wwwroot/rebrand'));
+});
 
-gulp.task('build-frontend', gulp.series('compile-scss', 'copy-fonts', 'copy-images', 'copy-javascript'))
+gulp.task('copy-javascript', gulp.series('copy-govuk-javascript', 'copy-custom-javascript'));
+
+gulp.task('build-frontend', gulp.series('compile-scss', 'copy-fonts', 'copy-images', 'copy-javascript', 'copy-rebrand'));
