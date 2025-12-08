ef296ad1c49aa9d4e4f3277dce0a6bcddc21e88d
paulb-esynergy <paul.barnard@esynergy.co.uk>
2025-12-08 17:26:25 +0000

    Added basic unit test implementation
    Co-Authored-By: Tim Abell <t@0x5.uk>

--- src/FrontendObligationChecker.UnitTests/Controllers/PublicRegisterLogicTests.cs
+++ src/FrontendObligationChecker.UnitTests/Controllers/PublicRegisterLogicTests.cs
@@ -1,14 +1,32 @@
 namespace FrontendObligationChecker.UnitTests.Controllers;
 
 using FrontendObligationChecker.Controllers;
+using FrontendObligationChecker.Models.BlobReader;
+using FrontendObligationChecker.ViewModels.PublicRegister;
 
 [TestClass]
 public class PublicRegisterLogicTests
 {
     [TestMethod]
-    public void TestGet()
+    public async Task TestGet()
     {
-        var actual = PublicRegisterController.GetRegisterViewModel(PublicRegisterController._urlOptions.PublicRegisterScottishProtectionAgency, PublicRegisterController._emailAddressOptions.DefraHelpline, PublicRegisterController._urlOptions.BusinessAndEnvironmentUrl, PublicRegisterController._urlOptions.DefraUrl, PublicRegisterController._options.PublishedDate, PublicRegisterController._options.PublicRegisterNextYearStartMonthAndDay, PublicRegisterController._options.PublicRegisterPreviousYearEndMonthAndDay, PublicRegisterController._options.CurrentYear);
+        var actual = await PublicRegisterController.GetRegisterViewModel(
+            isComplianceSchemesRegisterEnabled: true,
+            isEnforcementActionsSectionEnabled: true,
+            isPublicRegisterNextYearEnabled: true,
+            optionsCurrentYear: "2025",
+            optionsPublicRegisterPreviousYearEndMonthAndDay: "02-01",
+            optionsPublicRegisterNextYearStartMonthAndDay: "11-01",
+            optionsPublishedDate: new DateTime(2025, 9, 30),
+            urlOptionsDefraUrl: "https://defra.example.org",
+            urlOptionsBusinessAndEnvironmentUrl: "https://defra.example.org/business",
+            defraHelplineEmail: "help@example.org",
+            urlOptionsPublicRegisterScottishProtectionAgency: "https://defra.example.org/spa",
+            getUtcNow: () => new DateTime(2025, 12, 8),
+            getLatestFilePropertiesForPrefixes: _ => Task.FromResult(new Dictionary<string, PublicRegisterBlobModel>()),
+            getLatestFileProperties: () => Task.FromResult(new PublicRegisterBlobModel()),
+            getEnforcementActionFiles: () => Task.FromResult(new List<EnforcementActionFileViewModel>().AsEnumerable()));
+
         Assert.IsNotNull(actual);
     }
 }
