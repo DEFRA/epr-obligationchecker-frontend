fa8e44e7acd7bb5f1d9998ba4ed9178039c89784
Tim Abell <t@0x5.uk>
2025-12-09 16:48:04 +0000

    Update data tests to use FakeBlobStorageService

--- src/FrontendObligationChecker.UnitTests/Controllers/PublicRegisterLogicTests.cs
+++ src/FrontendObligationChecker.UnitTests/Controllers/PublicRegisterLogicTests.cs
@@ -89,25 +89,23 @@ public class PublicRegisterLogicTests
             .Excluding(x => x.Timestamp));
     }
 
-    /*
     [TestMethod]
-    [DataRow("2025-12-08", "2025", "2026", new[] { "2025", "2026" })]
-    [DataRow("2026-01-01", "2025", "2026", new[] { "2025", "2026" })]
-    [DataRow("2026-01-31", "2025", "2026", new[] { "2025", "2026" })]
-    [DataRow("2026-02-01", "2025", "2026", new[] { "2025", "2026" })]
-    [DataRow("2026-02-02", "2026", "2027", new[] { "2026" })]
-    public async Task TestDateBoundaryLogic(string now, string expectedCurrentYear, string expectedNextYear, string[] expectedPrefixes)
+    [DataRow("2025-12-08", "2025", "2026")]
+    [DataRow("2026-01-01", "2025", "2026")]
+    [DataRow("2026-01-31", "2025", "2026")]
+    [DataRow("2026-02-01", "2025", "2026")]
+    [DataRow("2026-02-02", "2026", "2027")]
+    public async Task TestDateBoundaryLogic(
+        string fakeCurrentDate, string expectedCurrentYear, string expectedNextYear)
     {
         // Arrange
-        var dateTime = ConvertToDateTime(now);
+        var utcNow = ConvertToDateTime(fakeCurrentDate);
 
         // Act
-        var guidanceViewModelResult = await GetGuidanceViewModel(dateTime);
+        var guidanceViewModel = await RunGetGuidanceViewModel(utcNow: utcNow);
 
         // Assert
-        guidanceViewModelResult.FolderPrefixes.Should().BeEquivalentTo(expectedPrefixes);
-
-        guidanceViewModelResult.ViewModel.Should().BeEquivalentTo(new GuidanceViewModel
+        guidanceViewModel.Should().BeEquivalentTo(new GuidanceViewModel
         {
             DefraUrl = "https://defra.example.org",
             BusinessAndEnvironmentUrl = "https://defra.example.org/business",
@@ -134,17 +132,17 @@ public class PublicRegisterLogicTests
             },
             ComplianceSchemeRegisteredFile = new PublicRegisterFileViewModel
             {
-                DatePublished = "30 September 2025",
-                DateLastModified = "7 December 2025",
+                DatePublished = null,
+                DateLastModified = null,
                 FileName = null,
-                FileSize = "0",
-                FileType = "CSV",
+                FileSize = null,
+                FileType = null,
             },
             EnforcementActionFiles = new List<EnforcementActionFileViewModel>(),
             EnglishEnforcementActionFile = null,
             WelshEnforcementActionFile = null,
             NortherIrishEnforcementActionFile = null,
-            ScottishEnforcementActionFileUrl = "https://defra.example.org/spa",
+            ScottishEnforcementActionFileUrl = null,
         }, options => options
             .Excluding(x => x.BackLinkToDisplay)
             .Excluding(x => x.CurrentPage)
@@ -156,15 +154,25 @@ public class PublicRegisterLogicTests
         return DateTime.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
     }
 
-    private static async Task<GuidanceViewModelResult> GetGuidanceViewModel(DateTime utcNow)
+    private static async Task<GuidanceViewModel> RunGetGuidanceViewModel(DateTime utcNow)
     {
-        var capturedPrefixes = new List<string>();
-
+        var fakeBlobStorageService = new FakeBlobStorageService([
+            new FakeBlob(
+                Name: "2025/Public_Register_Producers_27_November_2025.csv",
+                Properties: new FakeBlobProperties(
+                    ContentLength: 132629,
+                    LastModified: new DateTime(2025, 9, 30, 23, 59, 59, DateTimeKind.Utc))),
+            new FakeBlob(
+                Name: "2026/Public_Register_Producers_27_November_2025.csv",
+                Properties: new FakeBlobProperties(
+                    ContentLength: 25883,
+                    LastModified: new DateTime(2025, 12, 7, 23, 59, 59, DateTimeKind.Utc))),
+        ]);
         var getRegisterViewModel = await PublicRegisterController.GetRegisterViewModel(
-            isComplianceSchemesRegisterEnabled: true,
-            isEnforcementActionsSectionEnabled: true,
+            isComplianceSchemesRegisterEnabled: false,
+            isEnforcementActionsSectionEnabled: false,
             isPublicRegisterNextYearEnabled: true,
-            optionsCurrentYear: null,                   // This is null in Production, looks like it's only used for testing
+            optionsCurrentYear: null, // This is null in Production, looks like it's only used for testing
             optionsPublicRegisterPreviousYearEndMonthAndDay: "02-01",
             optionsPublicRegisterNextYearStartMonthAndDay: "11-01",
             optionsPublishedDate: new DateTime(2025, 9, 30, 0, 0, 0, DateTimeKind.Utc),
@@ -173,48 +181,13 @@ public class PublicRegisterLogicTests
             defraHelplineEmail: "help@example.org",
             urlOptionsPublicRegisterScottishProtectionAgency: "https://defra.example.org/spa",
             getUtcNow: () => utcNow,
-            getLatestFilePropertiesForPrefixes: folderPrefixes =>
-            {
-                capturedPrefixes = folderPrefixes;
-                return Task.FromResult(new Dictionary<string, PublicRegisterBlobModel>
-                {
-                    {
-                        "2025", new PublicRegisterBlobModel
-                        {
-                            ContentLength = "132629",
-                            EnforcementActionItems = null,
-                            FileType = "CSV",
-                            // yesterday
-                            LastModified = new DateTime(2025, 12, 7, 19, 59, 58, DateTimeKind.Utc),
-                            Name = "2025/Public_Register_Producers_27_November_2025.csv",
-                            // published previously (as seen in dev storage)
-                            PublishedDate = new DateTime(2025, 11, 3, 23, 57, 56, DateTimeKind.Utc)
-                        }
-                    },
-                    {
-                        "2026", new PublicRegisterBlobModel
-                        {
-                            ContentLength = "25883",
-                            EnforcementActionItems = null,
-                            FileType = "CSV",
-                            // yesterday
-                            LastModified = new DateTime(2025, 12, 7, 19, 45, 46, DateTimeKind.Utc),
-                            Name = "2026/Public_Register_Producers_27_November_2025.csv",
-                            // published previously (as seen in dev storage)
-                            PublishedDate = new DateTime(2025, 11, 3, 23, 59, 59, DateTimeKind.Utc)
-                        }
-                    },
-                });
-            },
-            getComplianceSchemeFileProperties: () => Task.FromResult(new PublicRegisterBlobModel()),
-            getEnforcementActionFiles: () => Task.FromResult(new List<EnforcementActionFileViewModel>().AsEnumerable()), blobStorageService: blobStorageService, optionsPublicRegisterBlobContainerName: PublicRegisterController._options.PublicRegisterBlobContainerName, optionsPublicRegisterCsoBlobContainerName: PublicRegisterController._options.PublicRegisterCsoBlobContainerName);
+            blobStorageService: fakeBlobStorageService,
+            optionsPublicRegisterBlobContainerName: "unused",
+            optionsPublicRegisterCsoBlobContainerName: "unused");
 
-        return new GuidanceViewModelResult(capturedPrefixes, getRegisterViewModel);
+        return getRegisterViewModel;
     }
-    */
 }
-
-public record GuidanceViewModelResult(List<string> FolderPrefixes, GuidanceViewModel ViewModel);
 
 /// <summary>
 /// Approximates the behaviour of <see cref="BlobStorageService"/> as of commit 645e5953854c9ab17c687296df1a57d797a45862 while avoiding the entanglement with azure blob storage apis.
